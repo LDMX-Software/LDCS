6a6459e4fbba9c34a6795d7d3b394f866610157c
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2021-05-06 13:55:27 -1000

    Add single script to set up the RTEs (including image building)

---
+++ helperScripts/setUpNewRTE.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ -z "$1" ] ;   then 
+	echo -e "Error: must specify name (the xyz of LDMX-xyz) for the RTE as first argument; exiting "
+	exit 1
+fi
+RTEname="LDMX-$1"
+RTEname=$(sed -e 's,LDMX-LDMX,LDMX,g' <<< $RTEname)
+echo "Using RTE name $RTEname"
+
+if [ -z "$2" ] ; then
+	swTag="v$1"
+	echo "Using default image naming scheme: vX.Y.Z"
+else
+	swTag="$2"
+fi
+echo "Using tag name $swTag"
+
+
+if [ -z "$3" ] ; then
+    swRepo=""
+    echo "No repo name provided; will rely on build script default"
+else
+    swRepo="$3"
+	echo "Using sw repo name $swTag"
+fi
+
+
+	
+startDir=${PWD}
+source sitePathSetup.config
+
+cd $LDCSPATH
+git fetch && git pull origin master
+
+
+cd $IMAGEPATH
+bash $LDCSPATH/images/build_from_docker.sh ${swTag} ${swRepo}
+imageBuildReturn=$?
+
+cd ${startDir}
+
+if [ ${imageBuildReturn} -ne 0 ] ; then
+	echo "Image building unsuccessful."
+	if [ "${FULLIMAGEPATH}" =  '.' ] && [ -f ${FULLIMAGEPATH} ] ; then
+		echo "Continuing setup with previously successfully built image ${FULLIMAGEPATH}."
+	else
+		echo -e "Error: singularity image ${FULLIMAGEPATH} not found! RTE not enabled."
+		exit $imageBuildReturn
+	fi
+fi
+
+cp ${LDCSPATH}/runTimeEnvironments/${RTEname} ${RTEPATH}/APPS/.
+sudo arcctl rte params-set ${RTEPATH}/APPS/${RTEname} SINGULARITY_IMAGE ${FULLIMAGEPATH}
+sudo arcctl rte enable ${RTEPATH}/APPS/${RTEname}
+

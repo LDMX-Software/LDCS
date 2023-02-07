7a081cf9756b077362eb648a2268fa40035019aa
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2023-02-07 17:15:02 +0100

    Initial commit of singularity image building executable
    This script is to be used to pull a container from dockerhub
    and convert it to a singularity image. It will take some metadata
    from a job config, and call the rucio helper script for output path
    generation, rucio cataloging of the image location and sw version
    metadata, etc.

---
+++ helperScripts/ldmxBuildImage.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+# Wrapper script for LDMX simulation
+#
+
+echo -e "ldmxBuildImage.sh running on host $(/bin/hostname -f)\n"
+
+# Check all files are present
+for f in "ldmxproduction.config" "build_from_user_docker.sh" "ldmx-simprod-rte-helper.py"; do
+  if [ ! -f "$f" ]; then
+    echo "ERROR: LDMX Simulation production job requires $f file but it is missing" >&2
+    exit 1
+  fi
+done
+
+echo -e "ldmxproduction.config:\n"
+cat ldmxproduction.config
+echo
+echo
+
+
+#this should not be needed 
+#if [ -z "$SINGULARITY_IMAGE" ]; then
+#  echo "ERROR: ARC CE admin should define SINGULARITY_IMAGE with arcctl rte params-set"
+#  exit 1
+#fi
+
+# Check singularity is installed
+type singularity
+if [ $? -ne 0 ]; then
+  echo "ERROR: Singularity installation on the worker nodes is required to run LDMX software"
+  exit 1
+fi
+
+# Initialise some parameters
+# potentially have a separate init function for image building 
+eval $( python3 ldmx-simprod-rte-helper.py -c ldmxproduction.config init )
+if [ -z "$OUTPUTDATAFILE" ]; then
+	echo "ERROR: Job config must define output image name"
+	exit 1
+fi
+echo -e "Output data file is $OUTPUTDATAFILE\n"
+if [ -z "${DOCKER_REPO}" ]; then
+    echo "ERROR: Job config must define input docker container repository"
+	exit 1
+echo -e "Using DockerHub repository ${DOCKER_REPO}"
+if [ -z "${DOCKER_TAG}" ]; then
+    echo "ERROR: Job config must define input docker container tag"
+	exit 1
+echo -e	"Using DockerHub container tag ${DOCKER_TAG}"
+
+# Copy over local replica to the worker node (singularity can't see unmounted dirs like storage)
+python3 ldmx-simprod-rte-helper.py -c ldmxproduction.config copy-local
+
+# The dependency versions are set in the docker container ldmx/dev
+# Parsing these versions out of that container is very difficult, so
+#   right now you need to provide them in the config, for porting to rucio.
+# You can find the docker build context for the development container
+#   in the GitHub repo LDMX-Software/docker and get all the details
+#   of the container using `docker inspect`
+
+export SINGULARITY_CACHEDIR=${PWD}/.singularity
+mkdir -p ${SINGULARITY_CACHEDIR} #make sure cache directory exists
+
+#build the image from the repo 
+image="${_image_name}.sif"
+location="docker://${DOCKER_REPO}:${DOCKER_TAG}"
+echo "pulling from DockerHub $location to build singularity image $image"
+
+singularity build ${image} ${location}
+RET=$?
+
+if [ $RET -ne 0 ]; then
+  echo "Singularity exited with code $RET"
+  exit $RET
+fi
+
+echo -e "\nSingularity exited normally, proceeding with post-processing...\n"
+
+# Post processing to extract metadata for rucio
+#eval $( python3 ldmx-simprod-rte-helper.py -j rucio.metadata -c ldmxproduction.config  collect-metadata )
+eval $( python3 ldmx-simprod-rte-helper.py -c ldmxproduction.config  collect-image-metadata )
+if [ ! -z "$KEEP_LOCAL_COPY" ]; then
+  if [ -z "$FINALOUTPUTFILE" ]; then
+    echo "Post-processing script failed!"
+    exit 1
+  fi
+  #the below should really only differ by a path 
+  echo "Copying $OUTPUTDATAFILE to $FINALOUTPUTFILE"
+  mkdir -p "${FINALOUTPUTFILE%/*}"
+  cp "$OUTPUTDATAFILE" "$FINALOUTPUTFILE"
+  if [ $? -ne 0 ]; then
+    echo "Failed to copy output to final destination"
+    exit 1
+  fi
+else
+  echo "KEEP_LOCAL_COPY is not set, not storing local copy of output"
+fi
+
+
+# Success
+echo "Success, exiting..."
+exit 0
+
